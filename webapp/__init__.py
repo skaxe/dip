0cdf9bbe78242e783455572fcc4e633c991a767e
Semyon Krasheninnikov <sa.krasheninnikov@yandex.ru>
2021-11-13 00:06:50 +0500

    Добавлен фронт енд

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, flash, redirect, url_for
 from webapp.random_predictor import RandomPredictor
 
@@ -22,10 +23,12 @@ def create_app():
         title = 'Результат предсказания'
         if form.validate_on_submit():
             return_number = random_predict.predict()
-            team1 = form.team_select1.data#Сюда возвращает None -> form.validate_on_submit() не дает зайти в этот блок
+            team1 = form.team_select1.data
             team2 = form.team_select2.data
+            team1_pic = f'images/clubs/{team1}.svg'
+            team2_pic = f'images/clubs/{team2}.svg'
             if (team1 != team2 and (team1 in form.team_list) and (team2 in form.team_list)):
-                return render_template('predict_page.html',form = form, return_number=return_number, page_title=title, team1=team1,team2=team2)
+                return render_template('predict_page.html',form = form, return_number=return_number, page_title=title, team1=team1,team2=team2, team1_pic=team1_pic, team2_pic=team2_pic)
             else:
                 return redirect(url_for('index'))
                 # надо сделать алерт что команда не та
--- webapp/forms.py
+++ webapp/forms.py
@@ -3,7 +3,11 @@ from wtforms import StringField, PasswordField, SubmitField, HiddenField, Select
 from wtforms.validators import DataRequired
 
 class SubmitForm(FlaskForm):
-    team_list = ['Team1', 'Team2', 'Team3', 'Team4']#по идее тянется из БД
+    team_list1222 = ['Team1', 'Team2', 'Team3', 'Team4']#по идее тянется из БД
+    team_list = ['Arsenal', 'Aston Villa', 'Bournemouth', 'Brentford', 'Brighton and Hove Albion', 'Burnley', 'Cardiff City', 'Chelsea',
+     'Crystal Palace', 'Everton', 'Fulham', 'Huddersfield Town', 'Leeds United', 'Leicester City', 'Liverpool', 'Manchester City',
+      'Manchester United', 'Newcastle United', 'Norwich City', 'Sheffield United', 'Southampton', 'Stoke City',
+       'Swansea City', 'Tottenham Hotspur', 'Watford', 'West Bromwich Albion', 'West Ham United', 'Wolverhampton Wanderers']
     team_select1 = SelectField('Выбрать команду 1',choices=team_list, validators=[DataRequired()], option_widget=None, validate_choice=True, render_kw={"class":"custom-select"})
     team_select2 = SelectField('Выбрать команду 2',choices=team_list, validators=[DataRequired()], render_kw={"class":"custom-select"} )
     submit = SubmitField('Отправить', render_kw={"class":"btn btn-primary"})
